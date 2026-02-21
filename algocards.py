991dd49f43a8b861dee7ba2ed16c0522b5f5e9b7
kluckovskatana <kluckovskatana@gmail.com>
2026-02-21 12:02:09 +0200

    cards

--- algocards.py
+++ algocards.py
@@ -1,6 +1,6 @@
 colors = ['G', 'R', 'B', 'Y']
 numbers = [str(i) for i in range(10)]
 
-result = [f"{num}_{color}" for color in colors for num in numbers]
+result = [f"{num}{color}" for color in colors for num in numbers]
 result += result
 print(result)
